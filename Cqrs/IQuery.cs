768214498f58f9adb2f211e4aa9753c105629e7f
Andrii Oliinyk <andrii.oliinyk@dentsplysirona.com>
2017-09-26 15:11:36 +0200

    Switch CQRS interface to self-executing

---
+++ Cqrs/IQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace Codelet.Cqrs
+{
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  /// <summary>
+  /// A query.
+  /// </summary>
+  /// <typeparam name="TResult">The type of the query result.</typeparam>
+  /// <typeparam name="TContext">The type of the query execution context.</typeparam>
+  public interface IQuery<TResult, in TContext>
+  {
+    /// <summary>
+    /// Executes this query.
+    /// </summary>
+    /// <param name="context">The query execution context.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The task that represents the process and produces query result.</returns>
+    Task<TResult> ExecuteAsync(TContext context, CancellationToken cancellationToken = default(CancellationToken));
+  }
+}
