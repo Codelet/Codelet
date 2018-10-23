2ada148335831b2c78738d6c73695d2eeb704944
Andrii Oliinyk <andrii.oliinyk@dentsplysirona.com>
2018-10-23 16:10:20 +0200

    Adjust emailing interface a bit

---
+++ Emailing/EmailsSender.cs
@@ -0,0 +1,14 @@
+﻿namespace Codelet.Emailing
+{
+  using System.Net.Mail;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  /// <summary>
+  /// Sends the <paramref name="email"/>.
+  /// </summary>
+  /// <param name="email">The email.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The task that represents the process.</returns>
+  public delegate Task EmailsSender(MailMessage email, CancellationToken cancellationToken = default);
+}
